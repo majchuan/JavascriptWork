3522f7aaf0ad042e43e4f17ea796e65e34bd0666
majincan@gmail.com <majincan@gmail.com>
2021-08-27 16:31:42 -0400

    leet code pratice

---
+++ leetCodeQ522.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {number}
+ */
+var findLUSlength = function(strs) {    
+    const strsSet = new Set(strs);
+    const strsArr = [...strsSet];
+
+    strsArr.sort((x,y) => x.length - y.length);
+    
+};
+
+const isSubsequence =(strA, strB) =>{
+    
+}
+
+const strts =["aabbcc", "aabbcc","cb","abc"];
+
