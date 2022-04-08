02cf4f27aed0f3c813bb6171abde20df1a1fcdee
Mark Ma <Mark.Ma3@ontario.ca>
2022-04-08 16:15:55 -0400

    leetcode practice

--- leetCodeQ378.js
+++ leetCodeQ378.js
@@ -16,18 +16,20 @@
 };
 
 var kthSmallestFastApproach = function (matrix, k) {
-    let low = matrix[0][0]
-    let high = matrix[matrix.length - 1][matrix[0].length - 1] + 1
-    while (low < high) {
-      const mid = low + Math.floor((high - low) / 2)
-      let count = 0
-      for (let i = 0; i < matrix.length; i++)
-        for (let j = 0; j < matrix.length; j++)
-          if (matrix[i][j] <= mid) count++
-          else break
-  
-      if (count < k) low = mid + 1
-      else high = mid
-    }
-    return low
+  let lo = matrix[0][0];
+  let hi = matrix[matrix.length-1][matrix[0].length-1]; 
+
+  while (lo <= hi) {
+      let mid = lo + Math.floor((hi-lo)/2);
+      let count = 0;
+      for (let i = 0;i<matrix.length;i++) {
+          for (let j=0;j<matrix.length;j++) {
+              if (matrix[i][j] <= mid) count++;
+              else break;
+          }
+      }
+      if (count < k) lo = mid+1;
+      else hi = mid-1;
   }
+  return lo
+}
