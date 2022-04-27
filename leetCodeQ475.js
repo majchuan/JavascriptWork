56fbd43d96117a1bad73894f36c48833a21bd2e9
majincan@gmail.com <majincan@gmail.com>
2022-04-27 14:53:02 -0400

    475 heater second solution

--- leetCodeQ475.js
+++ leetCodeQ475.js
@@ -3,7 +3,31 @@
  * @param {number[]} heaters
  * @return {number}
  */
+/*
+*Tow pointers
+*/
  var findRadius = function(houses, heaters) {
+    let length = heaters.length;
+    let j = 0;
+    let radius = 0;
+    
+    houses.sort((x,y) => x-y);
+    heaters.sort((x,y) => x-y);
+    
+    for(let house of houses){
+        while(j < length -1 && (Math.abs(heaters[j+1] - house) <= Math.abs(heaters[j] - house))){
+            j++;
+        }
+        
+        radius = Math.max(radius,Math.abs(heaters[j] -house));
+    }
+    
+    return radius;
+};
+/*
+*Binary Search
+*/
+ var findRadiusBinarySearchApproach = function(houses, heaters) {
     let radius = 0;
     let heaterLength = heaters.length;
     houses.sort((x,y) => x -y );
