39d9708c9bddefec7bf33c8fd2225e7751da59bf
Mark Ma <Mark.Ma3@ontario.ca>
2022-08-16 14:24:44 -0400

    practice

--- leetCodeQ387.js
+++ leetCodeQ387.js
@@ -2,7 +2,24 @@
  * @param {string} s
  * @return {number}
  */
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var firstUniqChar = function(s) {
+    let set = new Set();
+    
+    for(let i = 0; i < s.length; i++) {
+        if(s.indexOf(s[i], i + 1) == -1 && !set.has(s[i])) {
+            return i;
+        }
+        set.add(s[i]);
+    }
+    
+    return -1;
+};
+
+var firstUniqCharSlowApproach = function(s) {
     const hash_s ={};
     for(let i = 0 ; i < s.length ; i++){
         if(hash_s[s[i]] != null){
