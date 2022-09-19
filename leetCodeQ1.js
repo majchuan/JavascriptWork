ca4099c9183ae5c9eeaa113a473c520aaeb7faec
Mark Ma <Mark.Ma3@ontario.ca>
2022-09-19 13:27:11 -0400

    javascript leetcode practice

--- leetCodeQ1.js
+++ leetCodeQ1.js
@@ -18,3 +18,31 @@ var twoSum = function(nums, target) {
 };
 const nums = [2,7,11,15], target = 9;
 console.log(twoSum(nums,target));
+
+/*
+*Check nums array, two of memeber sum equal to target number. 
+*using two pointer approach to solve this issue.
+*/
+
+var isTwoSum= (nums, target)=>{
+    let left  = 0 ;
+    let right = nums.length -1 ;
+    nums.sort((x,y) => x-y);
+
+    while(left < right){
+        if(nums[left] + nums[right] == target){
+            return true;
+        }
+
+        if(nums[left] + nums[right] > target){
+            right--;
+        }else{
+            left++;
+        }
+    }
+
+    return false;
+}
+
+const nums1 =[7,3,11,15], target1 = 9;
+console.log(isTwoSum(nums1,target1));
