f63cf8ab24fb53e53ac8765726bd6875412b7a1c
majincan@gmail.com <majincan@gmail.com>
2022-10-05 16:03:01 -0400

    javascript practice

--- leetCodeQ2351.js
+++ leetCodeQ2351.js
@@ -16,3 +16,22 @@
     
     return "";
 };
+
+const sortString = (s) =>{
+    const letters = new Array(26).fill(0);
+    let result ="";
+    for(let i = 0 ; i < s.length; i++){
+        letters[s[i].charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    }
+
+    for(let i = 0; i < 26; i++){
+        for(let j = 0 ; j < letters[i]; j++){
+            result += String.fromCharCode(i + 'a'.charCodeAt(0));
+        }
+    }
+
+    return result;
+}
+
+const s = "bbccdefbbaa";
+console.log(sortString(s)) ; 
