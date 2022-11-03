e74e706a963161c1ca82b162bd0b9a92d06f8898
majincan@gmail.com <majincan@gmail.com>
2022-11-02 21:09:59 -0400

    javascript practice

---
+++ leetCodeQ445.js
@@ -0,0 +1,87 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+ var addTwoNumbers = function(l1, l2) {
+    const stack1 = [];
+    const stack2 = [];
+    const stackSum =[];
+    
+    while(l1 != null){
+        stack1.push(l1.val);
+        l1 = l1.next;
+    }
+    
+    while(l2 != null){
+        stack2.push(l2.val);
+        l2 = l2.next;
+    }
+    
+    let carry = 0;
+    while(stack1.length > 0 && stack2.length > 0){
+        let sum = stack1.pop() + stack2.pop() + carry;
+        if(sum > 9){
+            carry = 1;
+            sum = sum % 10
+        }else{
+            carry = 0 ;
+        }
+        
+        stackSum.unshift(sum);
+    }
+    
+    while(stack1.length > 0){
+        if(carry > 0){
+            let sum = stack1.pop() + carry ;
+            if(sum > 9){
+                carry = 1;
+                sum = sum % 10;
+                stackSum.unshift(sum);
+            }else{
+                carry = 0;
+                stackSum.unshift(sum);
+            }
+        }else{
+            stackSum.unshift(stack1.pop());
+        }
+    }
+    
+    
+    while(stack2.length > 0){
+        if(carry > 0){
+            let sum = stack2.pop() + carry ;
+            if(sum > 9){
+                carry =1;
+                sum = sum % 10;
+                stackSum.unshift(sum);
+            }else{
+                carry=0;
+                stackSum.unshift(sum);
+            }
+            
+        }else{
+            stackSum.unshift(stack2.pop());
+        }
+    }
+    
+    if(carry > 0){
+        stackSum.unshift(carry);
+    }
+    
+    let head = new ListNode(stackSum[0]);
+    let currNode = head;
+    for(let i = 1; i < stackSum.length; i++ ){
+        currNode.next = new ListNode(stackSum[i]);
+        currNode = currNode.next;
+    }
+    
+    return head;
+};
