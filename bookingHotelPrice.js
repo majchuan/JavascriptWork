747f1510cb6dc6317d27ca578085a7371e6d5a59
majincan@gmail.com <majincan@gmail.com>
2023-03-15 12:02:33 -0400

    booking.com code interview

---
+++ bookingHotelPrice.js
@@ -0,0 +1,102 @@
+/*
+*Booking hotels,
+*there is the list of hotels with price. user has a every mutiple days to visited the citys, assume there is only one hotel in the citys. 
+*to find out all travel routes which match the travel budget. 
+*user input:
+*hotels :{
+    paris :[30,20,120,50,90],
+    london:[50, 120, 90, 70,220],
+    amsterdam:[90,120,150,143,110]
+}
+userTravelInCity = 2 days,
+budget = 580;
+*/
+
+const travelInBudgets =(hotels,userTravelInCity, budget) =>{
+    const citys = Object.keys(hotels);
+    const trips = [];
+    const visited = new Set();
+    dfsCityAndPrice(hotels, citys, trips, visited, userTravelInCity, [], budget);
+    for(let t of trips){
+        console.log(t[0], t[1], t[2]);
+    }
+    return trips;
+}
+
+const dfsCityAndPrice = (hotels, citys, trips, visited, userTravelInCity, totalBudgets, budget) =>{
+    if(visited.size == citys.length){
+        let sum = totalBudgets.reduce((x,y) => x + y[1] , 0);
+        if(sum < budget ){
+            trips.push([[...visited], totalBudgets.map(x => x[0]), sum]);
+        }
+        return;
+    }
+
+    for(let city of citys){
+        if(visited.has(city)) continue;
+        visited.add(city);
+        for(let i = 0 ; i < hotels[city].length -1 ; i++){
+            let j = i;
+            let days = hotels[city].slice(i, i+ userTravelInCity).map(x => {
+                return {day : j++ , price : x } ;
+            });
+            let daysSum = days.reduce((x,y) => x + y.price,0);
+            totalBudgets.push([days, daysSum]);
+            dfsCityAndPrice(hotels, citys, trips, visited, userTravelInCity, totalBudgets,budget);
+            totalBudgets.pop();
+        }
+        visited.delete(city);
+    }
+}
+const dfsCity=(hotels, citys , trips, visited)=>{
+
+    if(visited.size == citys.length){
+        trips.push([...visited]);
+        return;
+    }
+
+    for(let city of citys){
+        if(visited.has(city)) continue;
+        visited.add(city);
+        dfsCity(hotels, citys, trips, visited);
+        visited.delete(city);
+    }
+}
+
+const dfsPrice = (hotels, trip, tripIndex, userTravelInCity, budget, totalBudgets, result)=>{
+    
+    if(totalBudgets.length == trip.length){
+        let totalSum = 0;//totalBudgets.reduce((x,y) => x[1] + y[1],0);
+        for(let tb of totalBudgets){
+            totalSum += tb[1];
+        }
+        if(totalSum <= budget){
+            result.push([trip,totalBudgets.slice(), totalSum]);
+        }
+        return;
+    }
+
+    for(let i = tripIndex ; i < trip.length ; i++){
+        const prices = hotels[trip[i]];
+        for(let j = 0 ; j < prices.length -1; j++){
+            let days= prices.slice(j, j+userTravelInCity);
+            let sum = days.reduce((x,y) => x+y,0);
+            totalBudgets.push([days,sum]);
+            dfsPrice(hotels, trip, i+1 , userTravelInCity, budget, totalBudgets, result);
+            totalBudgets.pop();
+        }
+    }
+    return ;
+}
+
+const hotels ={
+    paris :[30,20,120,50,90],
+    london:[50, 120, 90, 70,220],
+    amsterdam:[90,120,150,143,110]
+};
+const userTravelInCity = 2;
+const budget = 480;
+
+
+//console.log(travelInBudgets(hotels, userTravelInCity, budget));
+travelInBudgets(hotels,userTravelInCity,budget);
