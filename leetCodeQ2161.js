0dd0a56edfa31de14512694cee5e1c7e94db6e5f
Mark Ma <Mark.Ma3@ontario.ca>
2023-04-03 15:42:34 -0400

    Q2161 Two Pointers, Simulation

---
+++ leetCodeQ2161.js
@@ -0,0 +1,63 @@
+/**
+ * @param {number[]} nums
+ * @param {number} pivot
+ * @return {number[]}
+ */
+/*
+*Two Pointers
+*/
+var pivotArrayTwoPointers = function(nums, pivot) {
+    const result = [];
+    let left = 0 , right = nums.length -1;
+    let before = 0 ;
+    let after = nums.length -1;
+
+    while(left < nums.length && right >=0){
+        if(nums[left] < pivot){
+            result[before] = nums[left];
+            before++;
+        }
+
+        if(nums[right] > pivot){
+            result[after] = nums[right];
+            after--;
+        }
+
+        left++;
+        right--;
+    }
+
+    for(let i = before ; i <= after; i++){
+        result[i]=pivot;
+    }
+
+    return result;
+};
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} pivot
+ * @return {number[]}
+ */
+var pivotArray = function(nums, pivot) {
+    const leftNums = [];
+    const rightNums =[];
+    const pivots = [];
+
+    for(let i = 0 ; i < nums.length; i++){
+        if(nums[i] < pivot){
+            leftNums.push(nums[i]);
+        }
+
+        if(nums[i] == pivot){
+            pivots.push(nums[i]);
+        }
+
+        if(nums[i] > pivot){
+            rightNums.push(nums[i]);
+        }
+    }
+
+    return [...leftNums, ...pivots, ...rightNums];
+};
