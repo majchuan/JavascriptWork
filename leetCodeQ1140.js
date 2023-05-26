dfc04a6fb0cf039a97de9203fd4c4b7fd4d755e2
majincan@gmail.com <majincan@gmail.com>
2023-05-26 15:16:46 -0400

    Q1140 More clean solution

--- leetCodeQ1140.js
+++ leetCodeQ1140.js
@@ -4,39 +4,55 @@
  */
 var stoneGameII = function(piles) {
     const n = piles.length;
-    const memo = new Map();
-    const sums = new Array(n).fill(0);
-	
-    sums[n - 1] = piles[n - 1];
-    
-    for (let i = n - 2; i >= 0; i--) {
-        sums[i] = sums[i + 1] + piles[i]; // (1)
+    // Create a 2D array to store the maximum number of stones Alice can get
+    const dp = new Array(n);
+    for (let i = 0; i < n; i++) {
+        dp[i] = new Array(n);
+        dp[i].fill(0);
     }
     
-    function findMax(index, M) { // (2)
-        const key = `${index}#${M}`;
-
-        // base case
-        if (n - index <= 2 * M) {
-            return sums[index]; // (3)
-        }
-        if (memo.has(key)) return memo.get(key);
-        
-        let res = Number.MIN_SAFE_INTEGER;
-        
-        for (let x = 1; x <= 2 * M; x++) { // (4)
-            const newM = Math.max(x, M); // (5)
-            let maxValue = findMax(index+x, newM);
-            res = Math.max(res, sums[index] - findMax(index+x, newM)); // (6)
-
+    // Calculate the suffix sums for efficient calculation of remaining piles
+    const suffixSum = new Array(n + 1);
+    suffixSum[n] = 0;
+    for (let i = n - 1; i >= 0; i--) {
+        suffixSum[i] = suffixSum[i + 1] + piles[i];
+    }
+    
+    // Recursive function to calculate the maximum number of stones Alice can get
+    const dfs = (i, M) => {
+        // Base case: If all piles have been taken, return 0
+        if (i >= n) {
+            return 0;
         }
         
-        memo.set(key, res);
-        return res;
-    }
-    let result = findMax(0,1);
-    for(let [key, value ] of memo){
-        console.log(key, value);
-    }
-    return result;
+        // If the current state has already been calculated, return the stored result
+        if (dp[i][M] > 0) {
+            return dp[i][M];
+        }
+        
+        let maxStones = 0;
+        
+        // Try all possible choices of X for the current turn
+        for (let x = 1; x <= 2 * M; x++) {
+            // Calculate the remaining piles after taking X stones
+            const remaining = n - i - x;
+            
+            // Calculate the maximum number of stones the other player can get in the next turn
+            const otherPlayerStones = dfs(i + x, Math.max(M, x));
+            
+            // Calculate the maximum number of stones Alice can get in the current turn
+            const aliceStones = suffixSum[i] - otherPlayerStones;
+            
+            // Update the maximum number of stones Alice can get
+            maxStones = Math.max(maxStones, aliceStones);
+        }
+        
+        // Store the result in the dp array
+        dp[i][M] = maxStones;
+        
+        return maxStones;
+    };
+
+    // Start the recursive function from the beginning of the array with M = 1
+    return dfs(0,1);
 };
