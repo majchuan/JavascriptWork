a7e529e6ae4282d712eebbf46a34df42cad856d4
Mark Ma <Mark.Ma3@ontario.ca>
2023-06-05 17:01:30 -0400

    Q275, Binary search

---
+++ leetCodeQ275.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function(citations) {
+    let left = 0 ;
+    let right = citations.length - 1; 
+    
+    while(left <= right){
+        let mid = Math.floor((left+right)/2);
+        if(citations[mid] == citations.length - mid) return citations[mid];
+        if(citations[mid] > citations.length - mid){
+            right = mid - 1;
+        }else{
+            left = mid+1;
+        }
+    }
+
+    return citations.length - left ;
+};
