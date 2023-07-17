6a96a2b68c27c5d0ac5f1308953eb05121c5e86e
majincan@gmail.com <majincan@gmail.com>
2023-07-17 09:36:21 -0400

    Q445, Add new faster approach.

--- leetCodeQ445.js
+++ leetCodeQ445.js
@@ -85,3 +85,42 @@
     
     return head;
 };
+
+/**
+* Definition for singly-linked list.
+* function ListNode(val, next) {
+*     this.val = (val===undefined ? 0 : val)
+*     this.next = (next===undefined ? null : next)
+* }
+*/
+/**
+* @param {ListNode} l1
+* @param {ListNode} l2
+* @return {ListNode}
+*/
+var addTwoNumbersFasterApproach = function(l1, l2) {
+   const stack1=[];
+   const stack2=[];
+   while(l1 != null){
+       stack1.push(l1.val);
+       l1 = l1.next;
+   }
+
+   while(l2 != null){
+       stack2.push(l2.val);
+       l2 = l2.next;
+   }
+
+   let result = null;
+   let carry = 0;
+   while(stack1.length > 0 || stack2.length > 0 || carry){
+       let sum = 0;
+       if(stack1.length > 0) sum+=stack1.pop();
+       if(stack2.length > 0) sum+=stack2.pop();
+       sum += carry ;
+       carry = Math.floor(sum / 10);
+       result = new ListNode(sum % 10, result); // reverse linked list, from last node start to create, and set it pointer last node. 
+   }
+
+   return result;
+};
