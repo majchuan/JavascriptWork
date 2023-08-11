c42bb1681a923f16f676bd46e7eaad0ceb36d351
majincan@gmail.com <majincan@gmail.com>
2023-08-11 10:54:54 -0400

    Q322, DP with memo

---
+++ leetCodeQ322.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    const visited = {};
+    const result = dfs(coins, amount, visited) ;
+    return result == Number.MAX_SAFE_INTEGER+1 ? -1 : result;
+};
+
+const dfs=(coins ,amount, visited) =>{
+    if(visited[amount] != null) return visited[amount];
+
+    if(amount == 0) return 0;
+
+    let minSteps = Number.MAX_SAFE_INTEGER;
+
+    for(let coin of coins){
+        if(amount - coin >= 0 ){
+            minSteps = Math.min(minSteps, dfs(coins, amount - coin , visited));
+        }
+    }
+    visited[amount] = minSteps + 1;
+
+    return minSteps+1;
+}
