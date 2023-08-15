6b97fbe11eb8efe49e55ec60aed3bd0a1a7b830e
Mark Ma <Mark.Ma3@ontario.ca>
2023-08-15 11:25:27 -0400

    Q2161, Two Pointers

--- leetCodeQ2161.js
+++ leetCodeQ2161.js
@@ -61,3 +61,36 @@ var pivotArray = function(nums, pivot) {
 
     return [...leftNums, ...pivots, ...rightNums];
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} pivot
+ * @return {number[]}
+ */
+var pivotArrayTwoPointers = function(nums, pivot) {
+    const result = [];
+    let left = 0 , right = nums.length -1;
+    let before = 0 ;
+    let after = nums.length -1;
+
+    while(left < nums.length && right >=0){
+        if(nums[left] < pivot){
+            result[before] = nums[left];
+            before++;
+        }
+
+        if(nums[right] > pivot){
+            result[after] = nums[right];
+            after--;
+        }
+
+        left++;
+        right--;
+    }
+
+    for(let i = before ; i <= after; i++){
+        result[i]=pivot;
+    }
+
+    return result;
+};
