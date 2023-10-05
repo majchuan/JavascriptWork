993d0a26e485b663085736db49ba965db31af468
Mark Ma <Mark.Ma3@ontario.ca>
2023-10-05 14:15:14 -0400

    Q2284, FArray, hash, aster approach

--- leetCodeQ2284.js
+++ leetCodeQ2284.js
@@ -22,6 +22,38 @@ var largestWordCount = function(messages, senders) {
     return keys[0];
 };
 
+/**
+ * @param {string[]} messages
+ * @param {string[]} senders
+ * @return {string}
+ */
+var largestWordCountFastApproach = function(messages, senders) {
+    const hash_msg ={};
+    let maxFreq =  0 ; 
+    let result =''; 
+    for(let i = 0 ; i < senders.length ; i++){
+        let freq =  0 ; 
+
+        hash_msg[senders[i]] ? hash_msg[senders[i]] += wordsCount(messages[i]) : hash_msg[senders[i]] = wordsCount(messages[i]);
+
+        if(hash_msg[senders[i]] != null){
+            hash_msg[senders[i]] += wordsCount(messages[i]);
+            freq= hash_msg[senders[i]];
+        }else{
+            hash_msg[senders[i]] = wordsCount(messages[i]) ;
+            freq = wordsCount(messages[i]);
+        }
+
+        if(freq > maxFreq || (freq == maxFreq && senders[i] > result)){
+            maxFreq = freq;
+            result =senders[i];
+        }
+    }
+
+    return result;
+
+};
+
 const wordsCount=(message) =>{
     return message.split(' ').length;
 }
