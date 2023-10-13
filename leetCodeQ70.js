5bf0e611955814b1eb45b474d103a3e242027dfe
majincan@gmail.com <majincan@gmail.com>
2023-10-13 17:27:26 -0400

    Q70, Dynamic Programming

--- leetCodeQ70.js
+++ leetCodeQ70.js
@@ -2,6 +2,9 @@
  * @param {number} n
  * @return {number}
  */
+/*
+*bottom to up Dyanamic programming
+*/
 var climbStairs = function(n) {
     const fin = [1,1];
     for(let i = 2; i <= n ; i++){
@@ -13,3 +16,28 @@ var climbStairs = function(n) {
 
 const n = 10 ;
 console.log(climbStairs(n));
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+/*
+*Top to bottom Dynamic programming
+*/
+var climbStairs = function(n) {
+    const hash_n = new Map();
+    return dfs(n, hash_n);
+};
+
+const dfs=(n, hash_n) =>{
+    if(n == 0) return 0 ;
+    if(n == 1) return 1 ;
+    if(n == 2) return 2 ; 
+
+    if(hash_n.has(n)) return hash_n.get(n);
+
+    hash_n.set(n, dfs(n-1, hash_n) + dfs(n-2, hash_n));
+
+    return hash_n.get(n);
+}
