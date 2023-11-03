6623b53cabe4d4504f1e7913622034da0b81115a
majincan@gmail.com <majincan@gmail.com>
2023-11-03 10:12:16 -0400

    Q1441, Two Pointers.

--- leetCodeQ1441.js
+++ leetCodeQ1441.js
@@ -19,3 +19,27 @@ var buildArray = function(target, n) {
 
     return operation;
 };
+
+
+/**
+ * @param {number[]} target
+ * @param {number} n
+ * @return {string[]}
+ */
+var buildArrayTwoPoints = function(target, n) {
+    const operation=[];
+    let index = 0 ;
+    for(let i = 1 ; i <= n ; i++){
+        if(target[index] == i){
+            operation.push("Push");
+            index++;
+        }else{
+            operation.push("Push");
+            operation.push("Pop");   
+        }
+
+        if(index == target.length) break;
+    }
+
+    return operation
+};
