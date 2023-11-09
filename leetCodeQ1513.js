051cc402a10186f71daf80c1dc27901925b1348e
mark ma <39378154+majchuan@users.noreply.github.com>
2023-11-09 11:28:16 -0500

    Q1513.js, Math

---
+++ leetCodeQ1513.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numSub = function(s) {
+    let result = 0;
+    let start = 0 ;
+
+    for(let i = 0 ; i < s.length ; i++){
+        if( s[i] != s[start]){
+            let appear = i - start ;
+            while(appear > 0){
+                result += appear;
+                appear--;
+            }
+
+            start = i;
+        }
+
+        if(s[start] != 1) start++;
+    }
+
+    let appear = s.length - start ;
+    while(appear >0){
+        result += appear;
+        appear--;
+    }
+
+    return result % (Math.pow(10,9) + 7);
+};
