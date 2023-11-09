f300a9a2466911e4cc053e1b515ee00065e556c8
mark ma <39378154+majchuan@users.noreply.github.com>
2023-11-09 10:09:51 -0500

    Update leetCodeQ1759.js

--- leetCodeQ1759.js
+++ leetCodeQ1759.js
@@ -26,3 +26,23 @@ var countHomogenous = function(s) {
 
     return result % (Math.pow(10,9) + 7);
 };
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countHomogenousFastFunction = function(s) {
+    let i = 0,
+    res = 0,
+    cur = 0,
+    count = 0,
+    mod = 1_000_000_007;
+  while (i < s.length) {
+    count = s.charAt(i) === cur ? count + 1 : 1;
+    cur = s.charAt(i);
+    res = (res + count) % mod;
+    i++;
+  }
+  return res;
+};
