7fe728c38707093de02833e2678cfae34de17745
majincan@gmail.com <majincan@gmail.com>
2023-11-13 10:25:17 -0500

    Q2785, Update the name

--- leetCodeQ2785.js
+++ leetCodeQ2785.js
@@ -5,20 +5,20 @@
 var sortVowels = function(s) {
     const vowels = ['a','e','i','o','u'];
     const str = s.split('');
-    const hash_vowels =[];
+    const sortedVowels =[];
     const vowelsIndex =[];
 
     for(let i = 0 ; i < str.length ; i++){
         if(vowels.includes(str[i]) || vowels.includes(str[i].toLowerCase())){
-            hash_vowels.push(str[i]);
+            sortedVowels.push(str[i]);
             vowelsIndex.push(i);
         }
     }
 
-    hash_vowels.sort((a,b) => a.charCodeAt(0) - b.charCodeAt(0));
+    sortedVowels.sort((a,b) => a.charCodeAt(0) - b.charCodeAt(0));
 
-    for(let i = 0 ; i < hash_vowels.length ; i++){
-        str[vowelsIndex[i]] = hash_vowels[i];
+    for(let i = 0 ; i < sortedVowels.length ; i++){
+        str[vowelsIndex[i]] = sortedVowels[i];
     }
 
     return str.join('') ;
