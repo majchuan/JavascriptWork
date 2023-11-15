f359cf3cd297ee0b1c249a9a0e0473487b2ee6dd
majincan@gmail.com <majincan@gmail.com>
2023-11-15 09:25:04 -0500

    Q1846, Array, Sort, Greedy.

---
+++ leetCodeQ1846.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maximumElementAfterDecrementingAndRearranging = function(arr) {
+    arr.sort((a,b) => a - b );
+
+    if(arr[0] != 1) arr[0] = 1;
+    for(let i = 1 ; i < arr.length ; i++){
+        if(Math.abs(arr[i] - arr[i-1]) > 1){
+            arr[i] = arr[i-1] + 1;
+        }
+    }
+
+
+    return arr[arr.length - 1];
+};
