8b6bed81a5ec58866ccfafbc3ec2f96301083e84
majincan@gmail.com <majincan@gmail.com>
2023-11-24 16:03:26 -0500

    Q2614, Format

--- leetCodeQ2614.js
+++ leetCodeQ2614.js
@@ -24,14 +24,14 @@ var diagonalPrime = function(nums) {
   };
   
   //Seprate function for checkiing prime number
-  const isPrime=(num) => {
+const isPrime=(num) => {
     if (num < 2) {
-    return false;
-  }
-    for (let i = 2; i <= (num**0.5); i++) {
-      if (num % i === 0) {
         return false;
-      }
-  }
+    }
+    for (let i = 2; i <= (num**0.5); i++) {
+        if (num % i === 0) {
+        return false;
+        }
+    }
     return true;
-  }
+}
