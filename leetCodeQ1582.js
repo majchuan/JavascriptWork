e62c8a57a1ef1335395c247c237f30628e6f338c
majincan@gmail.com <majincan@gmail.com>
2023-12-12 19:51:53 -0500

    Q1582, Array,Matrix

---
+++ leetCodeQ1582.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var numSpecial = function(mat) {
+    let m = mat.length;
+    let n = mat[0].length;
+    const rowCount = new Array(m).fill(0);
+    const colCount = new Array(n).fill(0);
+
+    for(let i = 0 ; i < m ; i++){
+        for(let j = 0 ; j < n ; j++){
+            if(mat[i][j] == 1){
+                rowCount[i]++;
+                colCount[j]++;
+            }
+        }
+    }
+
+    let result = 0 ;
+
+    for(let i = 0; i< m ; i++){
+        for(let j = 0 ; j < n ; j++){
+            if(mat[i][j] == 1){
+                if(rowCount[i]== 1 && colCount[j] == 1){
+                    result++;
+                }
+            }
+        }
+    }
+
+    return result;
+};
