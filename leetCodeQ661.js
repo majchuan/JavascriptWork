7fdf4de7a16b0ec0647374e4a2691581bf926de1
majincan@gmail.com <majincan@gmail.com>
2023-12-18 21:27:02 -0500

    Q661, Array,Matrix.

---
+++ leetCodeQ661.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} img
+ * @return {number[][]}
+ */
+var imageSmoother = function(img) {
+    const m = img.length;
+    const n = img[0].length;
+
+    const smoothImg = new Array(m).fill().map( x => new Array(n).fill(0));
+    
+    for(let i = 0 ; i < m ; i++){
+        for(let j = 0; j < n ; j++){
+            let sum = 0 ;
+            let count = 0;
+
+            for(let x  = i-1; x <= i+1 ; x++){
+                for(let y = j-1; y <= j+1 ; y++){
+                    if(0 <= x && x < m && 0 <= y && y <n ){
+                        sum += img[x][y];
+                        count+=1;
+                    }
+                }
+            }
+
+            smoothImg[i][j] = Math.floor(sum / count);
+        }
+    }
+
+    return smoothImg;
+};
