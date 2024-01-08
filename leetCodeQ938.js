14d8670c98a80de6dafa9d92c522669d6cfaaa00
majincan@gmail.com <majincan@gmail.com>
2024-01-08 08:59:52 -0500

    Q938,Tree,DFS.

--- leetCodeQ938.js
+++ leetCodeQ938.js
@@ -43,3 +43,33 @@ const travseBST = (node,low, high,output) =>{
         }
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBSTFastApproach = function(root, low, high) {
+    let sum = 0;
+    if (root == null) {
+        return sum;
+    }
+
+    if(root.val >= low && root.val <= high){
+        sum += root.val
+    }
+    sum += rangeSumBST(root.left, low, high);
+    sum += rangeSumBST(root.right, low, high);
+
+    return sum;
+};
