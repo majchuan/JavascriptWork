0a904a2b056299461fabda4c4bad51b4557d62e6
mark ma <39378154+majchuan@users.noreply.github.com>
2024-01-15 14:12:43 -0500

    Update leetCodeQ2225.js

--- leetCodeQ2225.js
+++ leetCodeQ2225.js
@@ -10,22 +10,18 @@ var findWinners = function(matches) {
         winners[match[0]] ? winners[match[0]]++ : winners[match[0]] = 1 ;
         losers[match[1]] ? losers[match[1]]++ : losers[match[1]] = 1 ;
     }
-
     for(let winner in winners){
         if(losers[winner] == null){
             answer[0].push(winner);
         }
     }
-
     for(let loser in losers){
         if(losers[loser] == 1){
             answer[1].push(loser);
         }
     }
-
     answer[0].sort((a,b) => a-b);
     answer[1].sort((a,b) => a-b);
 
     return answer;
-
 };
