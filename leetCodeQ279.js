046cbdbadf78ee2819720c6cdbb25c25c65e316f
Mark Ma <Mark.Ma3@ontario.ca>
2024-02-08 11:04:42 -0500

    Q279, Upgrade

--- leetCodeQ279.js
+++ leetCodeQ279.js
@@ -10,7 +10,7 @@
     while( num * num <= n){
         let x = Math.floor(n / (num * num)) , y = n % (num*num);
 
-        result = Math.min(result, x + numSquares(y))
+        result = Math.min(result, x + numSquares(y));
 
         num++;
     }
@@ -22,20 +22,15 @@
 *Dyanmic programing.
 */
 var numSquaresDP = function(n) {
-    dp = new Array(n);
-
-    for(let i = 1 ; i <= n ; i++){
-        dp[i] = Number.MAX_SAFE_INTEGER; 
-    }
+    const dp = new Array(n+1).fill(Infinity);
     dp[0] = 0;
-    for(let i = 0; i <= n ; i++){
-        for(let j = 1 ; i+j*j <=n ; j++){
-            dp[i+j*j] = Math.min(dp[i+ j*j], dp[i] + 1 );
-            console.log(i+j*j, dp[i+j*j]);
+
+    for(let i = 1; i <= n; i++) {
+        for(let j = 1; j * j <= i; j++) {
+            dp[i] = Math.min(dp[i], dp[i - j*j] + 1);
         }
     }
-    console.log(dp);
-    return dp[n];
+    return dp[n] 
 };
 
 console.log(numSquaresDP(13));
