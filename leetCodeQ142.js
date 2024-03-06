7db40a34b865062949abd6f39b37c8bdb8b2e75e
majincan@gmail.com <majincan@gmail.com>
2024-03-05 20:11:32 -0500

    Q142, Standard approach to detect cycle start point.

--- leetCodeQ142.js
+++ leetCodeQ142.js
@@ -27,3 +27,26 @@
     
     return null;
 };
+
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle_1 = function(head) {
+    let slow = head;
+    let fast = head;
+
+    while(fast && fast.next){
+        fast = fast.next.next;
+        slow = slow.next;
+        if(fast == slow){
+            while(head != slow){
+                head = head.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+    }  
+    return null;
+};
