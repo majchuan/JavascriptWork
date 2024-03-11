8207f4ed0ea04d7466dfe1c51e6bbbf7e72b71c3
majincan@gmail.com <majincan@gmail.com>
2024-03-10 22:26:31 -0400

    Q791

--- leetCodeQ791.js
+++ leetCodeQ791.js
@@ -28,3 +28,46 @@ var customSortString = function(order, s) {
     
     return answer;
 };
+
+
+/**
+ * @param {string} order
+ * @param {string} s
+ * @return {string}
+ */
+var customSortStringSlowApproach = function(order, s) {
+    const orderS = [] ;
+    const hash_s ={};
+    s=s.split('').sort().join('');
+    for(let i = 0 ; i< s.length ; i++){
+        if(hash_s[s[i]] == null){
+            hash_s[s[i]] = [];
+            hash_s[s[i]].push(i);
+        }else{
+            hash_s[s[i]].push(i);
+        }
+    }
+    for(let i =0 ; i < order.length ; i++){
+        if(s.includes(order[i])){
+            orderS[i] = findAllString(order[i],hash_s);
+        }
+    }
+    let output = orderS.join('');
+    for(let item in hash_s){
+        for(let i = 0; i < hash_s[item].length ; i++){
+            output += item;
+        }
+    }
+    return output;
+};
+
+const findAllString = (repeaterS , hash_s) =>{
+    const length = hash_s[repeaterS].length;
+    let sumS = "";
+    for(let i= 0 ; i< length; i++){
+        sumS +=repeaterS;
+    }
+    delete hash_s[repeaterS];
+    return sumS;
+    
+}
