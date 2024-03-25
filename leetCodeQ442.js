213d17826da22c9af8d906fcbbcf52917c15b447
Mark Ma <Mark.Ma3@ontario.ca>
2024-03-25 11:25:26 -0400

    Q442, Use Mark visited index as minus value.

--- leetCodeQ442.js
+++ leetCodeQ442.js
@@ -43,3 +43,20 @@
     
     return duplicateNums;
 };
+
+
+const findDuplicates2 = (nums) => {
+    const duplicates = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        const index = Math.abs(nums[i]) - 1;
+        
+        if (nums[index] < 0) {
+            duplicates.push(index + 1);
+        }
+
+        nums[index] = nums[index] * -1;
+    }
+
+    return duplicates;
+};
