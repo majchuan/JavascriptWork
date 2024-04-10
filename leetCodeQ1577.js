ad9315058a1a625a18c32e87264743257c9c3691
majincan@gmail.com <majincan@gmail.com>
2024-04-10 15:17:25 -0400

    Q1577,Math,Hashtable,Two Pointers.

--- leetCodeQ1577.js
+++ leetCodeQ1577.js
@@ -6,8 +6,11 @@
 var numTriplets = function(nums1, nums2) {
     nums1.sort((a,b) => a - b);
     nums2.sort((a,b) => a - b);
-    let count = 0;
+    return calCulation(nums1, nums2) + calCulation(nums2, nums1);
+};
 
+const calCulation= (nums1, nums2) =>{
+    let count = 0;
     for(let i = 0 ; i < nums1.length ; i++){
         let left = 0 ; 
         let right = nums2.length-1;
@@ -18,7 +21,7 @@ var numTriplets = function(nums1, nums2) {
             if(square == product){
                   if(nums2[left] == nums2[right]){
                     count += (right - left) * (right - left + 1 ) / 2 ;
-                    left = right;
+                    break;
                 }else{
                     let x = left , y = right;
 
@@ -44,41 +47,5 @@ var numTriplets = function(nums1, nums2) {
         }
     }
 
-    for(let i = 0 ; i < nums2.length ; i++){
-        let left = 0 ; 
-        let right = nums1.length-1;
-        const square = nums2[i] * nums2[i];
-
-        while(left < right){
-            const product = nums1[left] * nums1[right];
-            if(square == product){
-                if(nums1[left] == nums1[right]){
-                    count += (right - left) * (right - left + 1 ) / 2 ;
-                    left = right;
-                }else{
-                    let x = left , y = right;
-
-                    while(left < right && nums1[left] == nums1[x]){
-                        left++;
-                    }
-
-                    while(right >= 0 && nums1[right] == nums1[y]){
-                        right--;
-                    }
-
-                    count += (left - x) * (y - right);
-                }
-            }
-
-            if(square > product){
-                left++;
-            }
-
-            if(square < product){
-                right--;
-            }
-        }
-    }
-
     return count;
-};
+}
