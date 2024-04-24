61a40a39e591f96322fcf43d6f68ddbe86eacd1a
majincan@gmail.com <majincan@gmail.com>
2024-04-23 20:48:14 -0400

    Q509,DP,DFS.Memo.

---
+++ leetCodeQ509.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fibMemo = function(n) {
+    const memo ={};
+    return dfs(n, memo);
+};
+
+const dfsMemo=(n,memo)=>{
+    if(n == 0) return 0 ;
+    if(n == 1) return 1;
+
+    memo[n] = dfs(n-1,memo) + dfs(n-2, memo);
+    return memo[n];
+}
+
+/**
+ * @param {number} n
+ * @return {number}
+ * Top to bottom DP, and dfs.
+ */
+var fib = function(n) {
+    const fibonacciSeq = [0,1];
+    for(let i = 2 ; i <= n ; i++){
+        fibonacciSeq[i] = fibonacciSeq[i-1] + fibonacciSeq[i-2];
+    }
+
+    return fibonacciSeq[n];
+};
