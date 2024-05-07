914eb0f88babceeca5302b4b262bf1140d0bd47e
majincan@gmail.com <majincan@gmail.com>
2024-05-07 10:20:59 -0400

    Q2816,Stack.

--- leetCodeQ2816.js
+++ leetCodeQ2816.js
@@ -28,3 +28,33 @@ var doubleIt = function(head) {
 
     return currHead;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var doubleItFastApproach = function(head) {
+    const st = [];
+    let d = 0;
+
+    while(head !== null){
+        st.push(head.val);
+        head = head.next;
+    }
+    while(st.length > 0){
+        let num = 2 * st.pop() + d;
+        d = num >= 10 ? 1 : 0;
+        head = new ListNode(num % 10, head);
+    }
+    if(d == 1)
+        head = new ListNode(d, head);
+    
+    return head;
+};
