5b0dd261729afe364c19c7b3f7a4e4e3232ca306
majincan@gmail.com <majincan@gmail.com>
2024-05-10 10:43:59 -0400

    Q786,Array,Two Pointers, Heap, Sorting,BinarySearch.

---
+++ leetCodeQ786.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number[]}
+ */
+var kthSmallestPrimeFraction = function(arr, k) {
+    let fractions = [], ans = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            fractions.push([arr[i] / arr[j], arr[i] , arr[j] ]);
+        }
+    }
+    fractions.sort((a, b) => a[0] - b[0]);
+    ans[0] = fractions[k-1][1];
+    ans[1] = fractions[k-1][2];
+    return ans
+};
