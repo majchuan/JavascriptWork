0fee71cb2f84b2a86869dbc061cd908e35556e57
majincan@gmail.com <majincan@gmail.com>
2024-05-29 09:24:00 -0400

    Q1404,Fast approach.

--- leetCodeQ1404.js
+++ leetCodeQ1404.js
@@ -24,5 +24,27 @@
     return step;
 };
 
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numStepsFastApproach = function(s) {
+    let result = carry = 0;
+ 
+    for (let index = s.length - 1; index > 0; index--) {
+       const str = s[index];
+ 
+       if (str === '0') {
+          result += carry ? 2 : 1;
+       } else {
+          result += carry ? 1 : 2;
+          carry = 1;
+       }
+ 
+    }
+    return result + carry; 
+ };
+
 const s = "1101";
 console.log(numSteps(s));
