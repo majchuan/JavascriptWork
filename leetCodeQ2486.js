4c20223213f148c941526461d89525ca66e52c31
mark ma <39378154+majchuan@users.noreply.github.com>
2024-06-03 09:48:46 -0400

    Update leetCodeQ2486.js

--- leetCodeQ2486.js
+++ leetCodeQ2486.js
@@ -15,3 +15,23 @@ var appendCharacters = function(s, t) {
 
     return t.length - right;
 };
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var appendCharactersSecondApproach = function(s, t) {
+    let left = 0;
+    let right = 0;
+
+    while(left < s.length){
+        while(s[left] == t[right] && left < s.length){
+            left++;
+            right++;
+        }
+        left++;
+    }
+
+    return t.length - right;
+};
