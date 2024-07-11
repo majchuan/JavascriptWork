8c8341c3629475d3b6470e86002d018c91245a95
mark ma <39378154+majchuan@users.noreply.github.com>
2024-07-11 11:29:40 -0400

    Update leetCodeQ1190.js

--- leetCodeQ1190.js
+++ leetCodeQ1190.js
@@ -19,3 +19,39 @@ var reverseParentheses = function(s) {
     }
     return stack.join("");
 };
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseParenthesesSlowFunction = function(s) {
+    const stack = [];
+    let result ="";
+    
+    for(let i = 0 ; i < s.length ; i++){
+        if(s[i] == '('){
+            stack.push(result.length);
+        }else if (s[i] ==')'){
+            let start = stack.pop();
+            result = reverse(result, start , result.length -1)
+        }else{
+            result += s[i];
+        }
+    }
+    
+    return result;
+};
+
+const reverse = (result, start , end) =>{
+    const sb = result.split('');
+    while(start < end){
+        let temp = sb[start];
+        sb[start] = sb[end];
+        sb[end] = temp;
+        start++;
+        end--;
+    }
+
+    return sb.join('');
+}
