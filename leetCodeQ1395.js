b0349641689e8133b570780b2c051b46f4b90e97
mark ma <39378154+majchuan@users.noreply.github.com>
2024-07-29 11:42:15 -0400

    Q1395,DP with Memo.

---
+++ leetCodeQ1395.js
@@ -0,0 +1,71 @@
+/**
+ * @param {number[]} rating
+ * @return {number}
+ */
+var numTeams = function(rating) {
+    let count = 0 ;
+    const result = [];
+    const cache_1 = new Array(rating.length+1).fill().map(() => new Array(5).fill(-1));
+    const cache_2 = new Array(rating.length+1).fill().map(() => new Array(5).fill(-1));
+    count += dfs_desc(rating, 0, result, cache_1);
+    count += dfs_incre(rating, 0, result, cache_2);
+    return count;
+};
+
+const dfs_desc=(rating, index, result, cache)=>{
+    let count = 0 ;
+    let length = result.length;
+
+    if(index == rating.length+1){
+        return 0 ;
+    }
+    if(result.length == 3){
+        return 1;
+    }
+
+    if(cache[index][length] != -1){
+        return cache[index][length];
+    }
+    for(let i = index ; i< rating.length; i++){
+        if(result.length > 0 && result[result.length -1] > rating[i]){
+            result.push(rating[i]);
+            count += dfs_desc(rating, i+1 , result, cache);
+            result.pop();
+        }else if(result.length == 0){
+            result.push(rating[i]);
+            count += dfs_desc(rating, i+1 ,result, cache);
+            result.pop();
+        }
+    }
+    cache[index][length] = count;
+    return count;
+}
+
+const dfs_incre=(rating, index, result, cache)=>{
+    let count = 0 ;
+    let length = result.length;
+
+    if(index == rating.length + 1){
+        return 0 ;
+    }
+
+    if(result.length == 3){
+        return 1;
+    }
+    if(cache[index][length] != -1){
+        return cache[index][length];
+    }
+    for(let i = index ; i< rating.length; i++){
+        if(result.length > 0 && result[result.length -1] < rating[i]){
+            result.push(rating[i]);
+            count += dfs_incre(rating, i+1 , result, cache);
+            result.pop();
+        }else if(result.length == 0){
+            result.push(rating[i]);
+            count += dfs_incre(rating, i+1 ,result, cache);
+            result.pop();
+        }
+    }
+    cache[index][length] = count;
+    return count;
+}
