2eeb0c33f667d18e451a609c3738b4f0a6938217
majincan@gmail.com <majincan@gmail.com>
2024-10-06 16:17:03 -0400

    Q1813,test case.

--- leetCodeQ1813.js
+++ leetCodeQ1813.js
@@ -12,7 +12,6 @@ var areSentencesSimilar = function(sentence1, sentence2) {
    let words2 = splitWords(sentence2);
 
    // Ensure words1 is the longer sentence
-   if (words1.length < words2.length) [words1, words2] = [words2, words1];
 
    let start = 0, end = 0;
    let n1 = words1.length, n2 = words2.length;
@@ -24,5 +23,10 @@ var areSentencesSimilar = function(sentence1, sentence2) {
    while (end < n2 && words1[n1 - end - 1] === words2[n2 - end - 1]) end++;
 
    // Check if the remaining unmatched part is in the middle
-   return start + end >= n2;
+   //console.log(n1,n2, start ,end);
+   return start + end >= Math.min(n1, n2);
 };
+
+//test case
+const sentence1 = 'Ogn WtWj HneS', sentence2='Ogn WtWj HneS';
+const sentence3 = 'I love day coding', sentence4='I love every coding';
