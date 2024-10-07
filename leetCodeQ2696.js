3efc0068d264f88a667b4c3edbf26a31c7672d34
majincan@gmail.com <majincan@gmail.com>
2024-10-07 09:51:23 -0400

    Q2696,Stack.

--- leetCodeQ2696.js
+++ leetCodeQ2696.js
@@ -16,3 +16,29 @@ var minLength = function(s) {
 
     return stack.length;
 };
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minLength1 = function(s) {
+    const stack = [];
+
+    for(let i = 0 ; i < s.length ; i++){
+        if(stack.length == 0) {
+            stack.push(s[i]);
+            continue;
+        }
+        while(stack.length > 0){
+            if(stack[stack.length-1].concat(s[i]) == 'AB' || stack[stack.length-1].concat(s[i]) == 'CD'){
+                stack.pop();
+                break;
+            }else{
+                stack.push(s[i]);
+                break;
+            }
+        }
+    }
+
+    return stack.length;
+};
