4a91f8a420992c70f9de44a595067534e5f13398
majincan@gmail.com <majincan@gmail.com>
2024-12-06 10:11:29 -0500

    Q2554,BinarySearch.

--- leetCodeQ2554.js
+++ leetCodeQ2554.js
@@ -19,3 +19,43 @@ var maxCount = function(banned, n, maxSum) {
     }
     return count;
 };
+
+
+/**
+ * @param {number[]} banned
+ * @param {number} n
+ * @param {number} maxSum
+ * @return {number}
+ */
+var maxCountBinarySearch = function(banned, n, maxSum) {
+    banned.sort((a,b) => a - b);
+    let count = 0 ; 
+    for(let i = 1 ; i <= n ; i++){
+        if(binarySearch(banned, i)) continue;
+
+        maxSum -= i;
+
+        if(maxSum < 0) break;
+
+        count++;
+    }
+
+    return count;
+};
+
+const binarySearch=(nums, target) =>{
+    let left = 0;
+    let right = nums.length -1;
+    while(left <= right){
+        let mid = Math.floor((left+right)/2);
+        if(nums[mid] == target) return true;
+
+        if(nums[mid] > target){
+            right = mid -1 ;
+        }else{
+            left = mid+1;
+        }
+    }
+
+    return false;
+}
