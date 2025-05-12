042c599acb7c4b2b69422ce9339e49966debad15
majincan@gmail.com <majincan@gmail.com>
2025-05-12 10:48:23 -0400

    Q2094, Fast approach.

--- leetCodeQ2094.js
+++ leetCodeQ2094.js
@@ -33,6 +33,33 @@ const dfs1=(digits, currNum ,visited, result)=>{
         }
     }
 }
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var findEvenNumbersFastApproach = function (digits) {
+    let bucket = new Array(10).fill(0);
+    let res = [];
+    for (let i=0; i<digits.length; i++) {
+        bucket[digits[i]]++;
+    }
+
+    for (let i=100; i<=999; i+=2) {
+        const hundred = Math.floor(i / 100);
+        const tens = Math.floor((i % 100) / 10);
+        const ones = i % 10;
+        let include = true;
+        if (--bucket[hundred] < 0) include = false;
+        if (--bucket[tens] < 0) include = false;
+        if (--bucket[ones] < 0) include = false;
+        if (include) res.push(i);
+        bucket[hundred]++; bucket[tens]++; bucket[ones]++;
+    }
+    return res;
+};
+
+
 const digits = [2,1,3,0];
 const digits1= [1,8,7,7,1,1,5,4,0,0,7,5,1,7,9]
 console.log(findEvenNumbers1(digits1));
