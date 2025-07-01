e0f5d29bdf322f5eb21f41c255c3ac8279109342
majincan@gmail.com <majincan@gmail.com>
2025-07-01 13:27:13 -0400

    Q3330,String.

---
+++ leetCodeQ3330.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var possibleStringCount = function(word) {
+    let n = word.length, ans = 1;
+    for (let i = 1; i < n; ++i) {
+        if (word[i - 1] === word[i]) {
+            ans++;
+        }
+    }
+    return ans;
+};
